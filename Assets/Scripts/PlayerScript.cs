fd6b4c121da0ede152916ab1edff0229c46fb12e
yellowchihuahua <47878958+yellowchihuahua@users.noreply.github.com>
2023-09-16 11:22:16 -0400

    animation scripting

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,6 +7,7 @@ public class PlayerScript : MonoBehaviour
     //references
     private Rigidbody2D rb2d;
     private LogicScript logic;
+    private Animator animator;
 
     //variables
     public float jumpStrength = 8f;
@@ -19,24 +20,36 @@ public class PlayerScript : MonoBehaviour
     void Start()
     {
         rb2d = this.GetComponent<Rigidbody2D>();
+        animator = this.GetComponent<Animator>();
+
 
         logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
-
     }
 
     // Update is called once per frame
     void Update()
     {
-
         UpdatePlayerState();
-        
 
+        UpdateAnimations();
         //add jump dampening in fixedupdate later or something
+    }
+
+    void UpdateAnimations()
+    {
+        animator.SetBool("_animatorIsGrounded", _isGrounded);
+
+        if (rb2d.velocity.y < -0.1f)
+        {
+            animator.SetBool("_animatorIsFalling", true);
+        } else
+        {
+            animator.SetBool("_animatorIsFalling", false);
+        }
     }
 
     void UpdatePlayerState()
     {
-
         _isGrounded = CheckGrounded();
 
         if (Input.GetKeyDown(KeyCode.Space) && _isPlayerAlive)
@@ -47,18 +60,15 @@ public class PlayerScript : MonoBehaviour
                 rb2d.velocity = Vector2.up * jumpStrength;
             }
         }
-
     }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Ouchy")
         {
-
             logic.gameOVer();
             _isPlayerAlive = false;
         }
-
-
     }
 
     private bool CheckGrounded()
