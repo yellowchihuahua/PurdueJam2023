a281ebadd73b784dd1595acec09029f4606dcc43
yellowchihuahua <47878958+yellowchihuahua@users.noreply.github.com>
2023-09-17 14:47:57 -0400

    UI consistency changes

--- Assets/Scripts/MenuSoundScript.cs
+++ Assets/Scripts/MenuSoundScript.cs
@@ -5,8 +5,9 @@ using UnityEngine.UI;
 
 public class MenuSoundScript : MonoBehaviour
 {
-    //this is no longer a sound script, just to fade the intro to white
+    //this is no longer a sound script, just to fade the intro to white and tutorial
     public GameObject fadeToWhitePanel;
+    public GameObject tutorialPanel;
     private bool _isTransitioning = false;
 
     private void Start()
@@ -19,6 +20,16 @@ public class MenuSoundScript : MonoBehaviour
         _isTransitioning = true;
     }
 
+    public void OpenTutorialPanel()
+    {
+        tutorialPanel.SetActive(true);
+    }
+
+    public void CloseTutorialPanel()
+    {
+        tutorialPanel.SetActive(false);
+    }
+
     private void Update()
     {
         if (_isTransitioning)
@@ -27,6 +38,7 @@ public class MenuSoundScript : MonoBehaviour
             image.color = new Color(255, 255, 255, image.color.a + (float)(1 * Time.deltaTime));
         }
     }
+
 
 
 }
